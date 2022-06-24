3bfc841359611819454651ee14c70f28e8e8c1dc
Fuller1212 <fullermorrison@gmail.com>
2022-06-24 16:57:37 -0500

    made functions cleaner and called functions

--- DTG.py
+++ DTG.py
@@ -11,8 +11,7 @@ entertainments = ['Golfing at TPC', 'Disneyworld', 'Tour a museum', 'Visit centr
 
 
 
-def get_random_destination():
-    # move original print statement into this function
+def choose_random_destination():
     print(f'We have selected {random.choice(destinations)} for your destination! Does this sound good?')
     answer = None
     while answer != 'yes': 
@@ -24,38 +23,41 @@ def get_random_destination():
            
             
 
-def get_random_restaurant(restaurant):
-    restaurant = None
-    while restaurant not in ('yes' , 'no'):
-        restaurant = input('yes or no ')
-        if restaurant == 'yes' or 'Yes' or 'y':
+def choose_random_restaurant():
+    print(f'We have selected {random.choice(restaurants)} for your restaurant! Does this sound good?')
+    answer = None
+    while answer != 'yes':
+        answer = input('yes or no ')
+        if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print(f'Great choice! Now lets move on!')
-        elif restaurant == 'no' or 'No' or 'n':
+        elif answer == 'no' or answer == 'No' or answer == 'n':
             print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(restaurants)} sound?')
-            input('yes or no')
+            
             
     
 
-def get_random_transportation(transportation):
-    transportation = None
-    while transportation not in ('yes' , 'no'):
-        transportation = input('yes or no ')
-        if transportation == 'yes' or 'Yes' or 'y':
+def choose_random_transportation():
+    print(f'We have selected {random.choice(transportations)} for your transportation! Does this sound good?')
+    answer = None
+    while answer != 'yes':
+        answer = input('yes or no ')
+        if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print('Great choice! Now lets move on!')
-        elif transportation == 'no' or 'No' or 'n':
+        elif answer == 'no' or answer == 'No' or answer == 'n':
             print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(transportations)} sound?')
-            input('yes or no')
+            
     
 
-def get_random_entertainment(entertainment):
-    entertainment = None
-    while entertainment not in ('yes' , 'no'):
-        entertainment = input('yes or no ')       
-        if entertainment == 'yes' or 'Yes' or 'y':
+def choose_random_entertainment():
+    print(f'We have selected {random.choice(entertainments)} for your entertainment! Does this sound good?')
+    answer = None
+    while answer != 'yes':
+        answer = input('yes or no ')       
+        if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print(f'Great choice! Now lets move on!')
-        elif entertainment == 'no' or 'No' or 'n':
+        elif answer == 'no' or answer == 'No' or answer == 'n':
             print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(entertainments)} sound?')
-            input('yes or no')
+            
             
     
         
@@ -63,41 +65,16 @@ def get_random_entertainment(entertainment):
 intro = 'Welcome to Day Trip Generator! If you are unsure or can not agree on where to vacation, then we can help fix that!'
 print(intro)
 
+choose_random_destination()
 
+choose_random_restaurant()
 
-# print(f'We have selected {random.choice(destinations)} for your destination! Does this sound good?')
+choose_random_transportation()
 
-get_random_destination()
-
-food = restaurants
-print(f'We have selected {random.choice(food)} for your restaurant! Does this sound good?')
-
-get_random_restaurant({random.choice(food)})
-
-trans = transportations
-print(f'We have selected {random.choice(trans)} for your transportation! Does this sound good?')
-
-get_random_transportation({random.choice(trans)})
-
-fun = entertainments
-print(f'We have selected {random.choice(fun)} for your entertainment! Does this sound good?')
-
-get_random_entertainment({random.choice(fun)})
+choose_random_entertainment()
 
 
 outro = "Congrats! Your day trip vacation has been planned. Let's just confirm that the trip information is correct."
 print(outro)
 
 print('The trip we have generated for you is:')
-
-Destination = destinations
-print('Destination:' + Destination)
-
-Restaurant = restaurants
-print('Restaurant:' + Restaurant )
-
-Transportation = trans
-print('Transportation:' + Transportation)
-
-Entertainment = fun
-print('Entertainment' + Entertainment)
