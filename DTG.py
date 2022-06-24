f8c0445f8d9578b7f1a99162198afeee89c5c96f
Fuller Morrison <fullermorrison@Wills-iMac-2.attlocal.net>
2022-06-24 15:55:22 -0500

    refactory random destination method

--- DTG.py
+++ DTG.py
@@ -5,65 +5,81 @@ destinations = ['Florida', 'United Kingdom', 'New York City', 'France']
 
 restaurants = ['Broadway Pizza', 'Ocean Prime', 'ASPIC', 'Pizza by the Sea']
 
-transportation = ['Rental car', 'Train', 'Uber', 'Biking',]
+transportations = ['Rental car', 'Train', 'Uber', 'Biking',]
 
-entertainment = ['Golfing at TPC', 'Disneyworld', 'Tour a museum', 'Go to central park']
-
-
-def get_random_destination(destination):
-    destination = input('yes or no ')           #need to create a loop that will continue until the answer is yes
-    if destination == 'yes':
-        print(f'Great choice! Now lets move on!')
-    elif destination == 'no':
-        print(f'Sorry you were not pleased with that option, let us try a different option. How does {destinations} sound?')
+entertainments = ['Golfing at TPC', 'Disneyworld', 'Tour a museum', 'Visit central park']
 
 
+
+def get_random_destination():
+    # move original print statement into this function
+    print(f'We have selected {random.choice(destinations)} for your destination! Does this sound good?')
+    answer = None
+    while answer != 'yes': 
+        answer = input('yes or no ') 
+        if answer == 'yes' or answer == 'Yes' or answer == 'y':
+            print(f'Great choice! Now lets move on!')
+        elif answer == 'no' or answer == 'No' or answer == 'n':
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(destinations)} sound?')
+           
+            
 
 def get_random_restaurant(restaurant):
-    restaurant = input('yes or no ')
-    if restaurant == 'yes':
-        print(f'Great choice! Now lets move on!')
-    elif restaurant == 'no':
-        print(f'Sorry you were not pleased with that option, let us try a different option. How does {restaurant} sound?')
-
+    restaurant = None
+    while restaurant not in ('yes' , 'no'):
+        restaurant = input('yes or no ')
+        if restaurant == 'yes' or 'Yes' or 'y':
+            print(f'Great choice! Now lets move on!')
+        elif restaurant == 'no' or 'No' or 'n':
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(restaurants)} sound?')
+            input('yes or no')
+            
+    
 
 def get_random_transportation(transportation):
-    transportation = input('yes or no ')
-    if transportation == 'yes':
-        print('Great choice! Now lets move on!')
-    elif transportation == 'no':
-        print(f'Sorry you were not pleased with that option, let us try a different option. How does {transportation} sound?')
-
+    transportation = None
+    while transportation not in ('yes' , 'no'):
+        transportation = input('yes or no ')
+        if transportation == 'yes' or 'Yes' or 'y':
+            print('Great choice! Now lets move on!')
+        elif transportation == 'no' or 'No' or 'n':
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(transportations)} sound?')
+            input('yes or no')
+    
 
 def get_random_entertainment(entertainment):
-    entertainment = input('yes or no ')         
-    if entertainment == 'yes':
-        print(f'Great choice! Now lets move on!')
-    elif entertainment == 'no':
-        print(f'Sorry you were not pleased with that option, let us try a different option. How does {entertainment} sound?')
-
-
+    entertainment = None
+    while entertainment not in ('yes' , 'no'):
+        entertainment = input('yes or no ')       
+        if entertainment == 'yes' or 'Yes' or 'y':
+            print(f'Great choice! Now lets move on!')
+        elif entertainment == 'no' or 'No' or 'n':
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(entertainments)} sound?')
+            input('yes or no')
+            
+    
+        
 
 intro = 'Welcome to Day Trip Generator! If you are unsure or can not agree on where to vacation, then we can help fix that!'
 print(intro)
 
 
-location = destinations
-print(f'We have selected {random.choice(location)} for your destination! Does this sound good?')
 
-get_random_destination({random.choice(location)})
+# print(f'We have selected {random.choice(destinations)} for your destination! Does this sound good?')
+
+get_random_destination()
 
 food = restaurants
 print(f'We have selected {random.choice(food)} for your restaurant! Does this sound good?')
 
 get_random_restaurant({random.choice(food)})
 
-trans = transportation
+trans = transportations
 print(f'We have selected {random.choice(trans)} for your transportation! Does this sound good?')
 
 get_random_transportation({random.choice(trans)})
 
-fun = entertainment
+fun = entertainments
 print(f'We have selected {random.choice(fun)} for your entertainment! Does this sound good?')
 
 get_random_entertainment({random.choice(fun)})
@@ -74,3 +90,14 @@ print(outro)
 
 print('The trip we have generated for you is:')
 
+Destination = destinations
+print('Destination:' + Destination)
+
+Restaurant = restaurants
+print('Restaurant:' + Restaurant )
+
+Transportation = trans
+print('Transportation:' + Transportation)
+
+Entertainment = fun
+print('Entertainment' + Entertainment)
---
+++ DayTripGenerator.py
@@ -0,0 +1,83 @@
+
+
+destinations = ['Florida', 'United Kingdom', 'New York City', 'France']
+
+restaurants_in_florida = ['Caretta on the Gulf', 'The Melting Pot', "Miami's Best Pizza", 'Ocean Prime']
+
+restaurants_in_united_kingdom = ['The Ledbury', 'Kitchen Table', 'Alchemilla', 'Tomo Pizzeria-Restaurant']
+
+restaurants_in_new_york_city = ['Capizzi', 'Marea', 'COTE Korean Steakhouse', 'Daniel']
+
+restaurants_in_france = ['ASPIC', 'Le Chantecler', 'Bistrot Instinct', 'Pizza Rossi']
+
+transportation = ['Rental car', 'Train', 'Uber', 'Biking',]
+
+entertainment_in_florida = ['Day on the beach', 'Golf at TPC Sawgrass', 'Amusement park', 'Tour The Dali museum']
+
+entertainment_in_united_kingdom = ['Tour Windsor Castle', 'Visit Tower of London', 'Tour The National Gallery of art', 'Ride the London Eye']
+
+entertainment_in_new_york_city = ['Tour the Museum of Modern Art', 'Visit the Statue of Liberty', 'Go to Central Park', 'Explore Times Square']
+
+entertainment_in_france = ['Tour the Eiffel Tower', 'Tour Louvre Museum', 'Go to Disneyland Paris', 'Ski at The Pyrenees']
+
+def get_random_destination(destniation):
+    if destniation == 'yes':
+        print(f'Great choice! {destniation} is a great place!')
+    elif destniation == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {destniation} sound?')
+
+def get_random_restaurant_in_florida(restaurant):
+    if restaurant == 'yes':
+        print(f'Great choice! {restaurant} is very delicious!')
+    elif restaurant == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {restaurant} sound?')
+
+def get_random_restaurant_in_united_kingdom(restaurant):
+    if restaurant == 'yes':
+        print(f'Great choice! {restaurant} is very delicious!')
+    elif restaurant == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {restaurant} sound?')
+
+def get_random_restaurant_in_new_york_city(restaurant):
+    if restaurant == 'yes':
+        print(f'Great choice! {restaurant} is very delicious!')
+    elif restaurant == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {restaurant} sound?')
+
+
+def get_random_restaurant_in_france(restaurant):
+    if restaurant == 'yes':
+        print(f'Great choice! {restaurant} is very delicious!')
+    elif restaurant == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {restaurant} sound?')
+
+def get_random_transportation(transportation):
+    if transportation == 'yes':
+        print('Great choice! Now lets move on!')
+    elif transportation == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {transportation} sound?')
+
+def get_random_entertainment_in_florida(entertainment):
+    if entertainment == 'yes':
+        print(f'Great choice! {entertainment} is a lot of fun')
+    elif entertainment == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {entertainment} sound?')
+
+def get_random_entertainment_in_united_kingdom(entertainment):
+    if entertainment == 'yes':
+        print(f'Great choice! {entertainment} is a lot of fun')
+    elif entertainment == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {entertainment} sound?')
+
+def get_random_entertainment_in_new_york_city(entertainment):
+    if entertainment == 'yes':
+        print(f'Great choice! {entertainment} is a lot of fun')
+    elif entertainment == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {entertainment} sound?')
+
+def get_random_entertainment_in_france(entertainment):
+    if entertainment == 'yes':
+        print(f'Great choice! {entertainment} is a lot of fun')
+    elif entertainment == 'no':
+        print(f'Sorry you were not pleased with that option, let us try a different option. How does {entertainment} sound?')
+
