a10a8c2130dc9f8ca85033992015aa7f71a047b2
Fuller1212 <fullermorrison@gmail.com>
2022-06-26 21:54:22 -0500

    Added to functions and created new variables

--- DTG.py
+++ DTG.py
@@ -9,72 +9,101 @@ transportations = ['Rental car', 'Train', 'Uber', 'Biking',]
 
 entertainments = ['Golfing at TPC', 'Disneyworld', 'Tour a museum', 'Visit central park']
 
+result = ['', '', '', '']
+destination =''
+restaurant = ''
+transportation = ''
+entertainment = ''
+
 
 
 def choose_random_destination():
-    print(f'We have selected {random.choice(destinations)} for your destination! Does this sound good?')
     answer = None
+    destination = random.choice(destinations)
+    print(f'We have selected {destination} for your destination! Does this sound good?')
+    result[0] = destination
     while answer != 'yes': 
         answer = input('yes or no ') 
         if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print(f'Great choice! Now lets move on!')
         elif answer == 'no' or answer == 'No' or answer == 'n':
-            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(destinations)} sound?')
+            destination = random.choice(destinations)
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {destination} sound?')
+    return destination
            
             
 
 def choose_random_restaurant():
-    print(f'We have selected {random.choice(restaurants)} for your restaurant! Does this sound good?')
     answer = None
+    restaurant = random.choice(restaurants)
+    result[1] = restaurant
+    print(f'We have selected {restaurant} for your restaurant! Does this sound good?')
     while answer != 'yes':
         answer = input('yes or no ')
         if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print(f'Great choice! Now lets move on!')
         elif answer == 'no' or answer == 'No' or answer == 'n':
-            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(restaurants)} sound?')
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {restaurant} sound?')
+    return restaurant 
             
             
     
 
 def choose_random_transportation():
-    print(f'We have selected {random.choice(transportations)} for your transportation! Does this sound good?')
     answer = None
+    transportation = random.choice(transportations)
+    result[2] = transportation
+    print(f'We have selected {random.choice(transportations)} for your transportation! Does this sound good?')
     while answer != 'yes':
         answer = input('yes or no ')
         if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print('Great choice! Now lets move on!')
         elif answer == 'no' or answer == 'No' or answer == 'n':
-            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(transportations)} sound?')
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {transportation} sound?')
+    return transportation 
             
     
 
 def choose_random_entertainment():
-    print(f'We have selected {random.choice(entertainments)} for your entertainment! Does this sound good?')
     answer = None
+    entertainment = random.choice(entertainments)
+    result[3] = entertainment
+    print(f'We have selected {random.choice(entertainments)} for your entertainment! Does this sound good?')
     while answer != 'yes':
         answer = input('yes or no ')       
         if answer == 'yes' or answer == 'Yes' or answer == 'y':
             print(f'Great choice! Now lets move on!')
         elif answer == 'no' or answer == 'No' or answer == 'n':
-            print(f'Sorry you were not pleased with that option, let us try a different option. How does {random.choice(entertainments)} sound?')
+            print(f'Sorry you were not pleased with that option, let us try a different option. How does {entertainment} sound?')
+    return entertainment
             
             
     
         
-
 intro = 'Welcome to Day Trip Generator! If you are unsure or can not agree on where to vacation, then we can help fix that!'
 print(intro)
 
-choose_random_destination()
+destination = choose_random_destination()
 
-choose_random_restaurant()
+restaurant = choose_random_restaurant()
 
-choose_random_transportation()
+transportation = choose_random_transportation()
 
-choose_random_entertainment()
+entertainment = choose_random_entertainment()
 
 
 outro = "Congrats! Your day trip vacation has been planned. Let's just confirm that the trip information is correct."
 print(outro)
 
 print('The trip we have generated for you is:')
+
+print(f'Destination: {destination}')
+
+print(f'Restaurant: {restaurant}')
+
+print(f'Transportation: {transportation}')
+
+print(f'Entertainment: {entertainment}')
+
+print(f'Prepare for the time of your life as you visit {destination} while traveling by {transportation}. Enjoying a nice dinner at {restaurant} and having a blast while {entertainment}')
+
